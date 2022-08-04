096bec0cb86753e05f125a851905ab2f747d9ae9
itisthecon <itisthecon@gmail.com>
2022-08-04 09:16:30 +0800

    add gitconfig

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ sudo apt-get update
 sudo apt-get -y install neovim
 
 rm -f ~/.bashrc
+
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s /usr/bin/nvim /usr/local/bin/
+
 . ~/.bashrc
