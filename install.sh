74eb9c68d48f9d418b1a95b3b670143578b3f3f1
itisthecon <itisthecon@gmail.com>
2022-08-06 12:20:47 +0800

    install script fix

--- install.sh
+++ install.sh
@@ -11,7 +11,9 @@ rm -f ~/.gitconfig
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -s /usr/bin/nvim ~/.local/bin/
-ln -s ~/.dotfiles/execs/exa ~/.local/bin/
+sudo ln -s /usr/bin/nvim /usr/local/bin/
+#ln -s ~/.dotfiles/execs/exa ~/.local/bin/
 
 . ~/.bashrc
+
+export PATH=$HOME/.dotfiles/execs:$PATH
