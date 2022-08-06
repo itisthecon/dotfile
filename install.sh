e218c0e201ab7642aef79d8187c54b8c3e9c42e5
itisthecon <itisthecon@gmail.com>
2022-08-06 12:17:15 +0800

    install script fix

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get -y install neovim
-sudo apt ugrade
+sudo apt -y ugrade
 
 rm -f ~/.bashrc
 rm -f ~/.gitconfig
@@ -12,6 +12,6 @@ ln -s ~/.dotfiles/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s /usr/bin/nvim ~/.local/bin/
-ln -s ~/dotfiles/execs/exa ~/.local/bin/
+ln -s ~/.dotfiles/execs/exa ~/.local/bin/
 
 . ~/.bashrc
