141b325f90ed112447068f978c18d912aa98e7f5
itisthecon <itisthecon@gmail.com>
2022-08-06 13:16:26 +0800

    install script nvim rc file fix

--- install.sh
+++ install.sh
@@ -1,21 +1,29 @@
 #/bin/sh
 
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt-get update
-sudo apt-get -y install neovim
-sudo apt -y upgrade
-
 rm -f ~/.bashrc
+ln -s ~/.dotfiles/.bashrc ~/.bashrc
+. ~/.bashrc
+export PATH=$HOME/.dotfiles/execs:$PATH
+
 rm -f ~/.gitconfig
 rm -f ~/.vimrc
 sudo rm -f /usr/local/bin/nvim
 
-ln -s ~/.dotfiles/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 sudo ln -s /usr/bin/nvim /usr/local/bin/
 #ln -s ~/.dotfiles/execs/exa ~/.local/bin/
 
-. ~/.bashrc
+if ! [[ -d $HOME/.config/nvim ]];
+then
+    mkdir -p $HOME/.config/nvim
+fi
+if ! [[ -f $HOME/.config/nvim/init.vim ]];
+then
+    ln -s ~/.dotfiles/.vimrc ~/.config/nvim/init.vim
+fi
 
-export PATH=$HOME/.dotfiles/execs:$PATH
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get -y install neovim
+sudo apt -y upgrade
